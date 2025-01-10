01f1ee2a753a234c6216a3ea2a6a53d6834936b4
Ben Allenden <benallenden@protonmail.com>
2025-01-10 00:45:14 +0000

    Implement AuthController handleInvalidRequestException (catches exceptions thrown from @Valid annotation)

--- src/main/java/com/example/echo_api/api/v1/auth/AuthControllerAdvice.java
+++ src/main/java/com/example/echo_api/api/v1/auth/AuthControllerAdvice.java
@@ -1,10 +1,15 @@
 package com.example.echo_api.api.v1.auth;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AccountStatusException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -17,6 +22,31 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @ControllerAdvice(assignableTypes = AuthController.class)
 public class AuthControllerAdvice extends AbstractControllerAdvice {
+
+    @ExceptionHandler({ MethodArgumentNotValidException.class })
+    ResponseEntity<?> handleInvalidRequestException(HttpServletRequest request, MethodArgumentNotValidException ex) {
+        log.debug("Handling exception: {}", ex.getMessage());
+
+        // build linked hashmap of MethodArgumentNotValidException field errors to
+        // maintain insertion order
+        List<Map<String, String>> details = ex
+                .getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> {
+                    Map<String, String> tmp = new LinkedHashMap<>();
+                    tmp.put("field", error.getField());
+                    tmp.put("message", error.getDefaultMessage());
+                    return tmp;
+                })
+                .toList();
+
+        return createExceptionHandler(
+                request,
+                HttpStatus.BAD_REQUEST,
+                "request is invalid",
+                details);
+    }
 
     @ExceptionHandler({ UsernameNotFoundException.class, BadCredentialsException.class })
     ResponseEntity<?> handleAuthenticationException(HttpServletRequest request, Exception ex) {
