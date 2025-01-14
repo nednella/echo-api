7e00d885f381e90f3dd9e77589089d09883b9963
Ben Allenden <benallenden@protonmail.com>
2025-01-14 15:39:57 +0000

    Replace Application CLR with loader package for importing test data

---
+++ src/main/java/com/example/echo_api/loader/Loader.java
@@ -0,0 +1,16 @@
+package com.example.echo_api.loader;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class Loader implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+    }
+
+}
