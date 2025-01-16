ddc106cac1884346e4f6057fb21bf6a99c30d69d
Ben Allenden <benallenden@protonmail.com>
2025-01-16 23:38:29 +0000

    Loader should only run in dev mode

--- src/main/java/com/example/echo_api/loader/Loader.java
+++ src/main/java/com/example/echo_api/loader/Loader.java
@@ -1,10 +1,12 @@
 package com.example.echo_api.loader;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
 
+@Profile("dev")
 @Component
 @RequiredArgsConstructor
 public class Loader implements CommandLineRunner {
