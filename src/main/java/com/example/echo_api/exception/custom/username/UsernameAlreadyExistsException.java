32506d2e2c109b1a298ddc3e4dbb679f8bcb5a6e
Ben Allenden <benallenden@protonmail.com>
2025-01-19 16:58:42 +0000

    Update jdocs

--- src/main/java/com/example/echo_api/exception/custom/username/UsernameAlreadyExistsException.java
+++ src/main/java/com/example/echo_api/exception/custom/username/UsernameAlreadyExistsException.java
@@ -1,7 +1,15 @@
 package com.example.echo_api.exception.custom.username;
 
+/**
+ * Thrown if a HTTP request is rejected because the supplied username already
+ * exists in the db.
+ */
 public class UsernameAlreadyExistsException extends UsernameException {
 
+    /**
+     * Constructs a {@code UsernameAlreadyExistsException} with the specified
+     * message.
+     */
     public UsernameAlreadyExistsException() {
         super("Username already exists.");
     }
--- src/main/java/com/example/echo_api/exception/custom/username/UsernameNotFoundException.java
+++ src/main/java/com/example/echo_api/exception/custom/username/UsernameNotFoundException.java
@@ -1,7 +1,19 @@
 package com.example.echo_api.exception.custom.username;
 
+/**
+ * Thrown if a HTTP request is rejected because the supplied username does not
+ * exist in the db.
+ * 
+ * <p>
+ * Note that this exception is intended to differ from
+ * {@link org.springframework.security.core.userdetails.UsernameNotFoundException}
+ * because it is not associated with the authentication flow.
+ */
 public class UsernameNotFoundException extends UsernameException {
 
+    /**
+     * Constructs a {@code UsernameNotFoundException} with the specified message.
+     */
     public UsernameNotFoundException() {
         super("Username not found.");
     }
