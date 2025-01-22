19c34173a56ec93f557519f350cda723652d1457
Ben Allenden <benallenden@protonmail.com>
2025-01-22 02:40:07 +0000

    Refactor @NotEmpty to @NotBlank

--- src/main/java/com/example/echo_api/persistence/dto/request/account/UpdatePasswordRequest.java
+++ src/main/java/com/example/echo_api/persistence/dto/request/account/UpdatePasswordRequest.java
@@ -3,21 +3,21 @@ package com.example.echo_api.persistence.dto.request.account;
 import com.example.echo_api.validation.Password;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 
 // @formatter:off
 public record UpdatePasswordRequest(
 
-    @NotEmpty(message = "Current password is required.")
+    @NotBlank(message = "Current password is required.")
     @JsonProperty("current_password")
     String currentPassword,
 
     @Password
-    @NotEmpty(message = "New password is required.")
+    @NotBlank(message = "New password is required.")
     @JsonProperty("new_password")
     String newPassword,
 
-    @NotEmpty(message = "Confirmation password is required.")
+    @NotBlank(message = "Confirmation password is required.")
     @JsonProperty("confirmation_password")
     String confirmationPassword
 
--- src/main/java/com/example/echo_api/persistence/dto/request/account/UpdateUsernameRequest.java
+++ src/main/java/com/example/echo_api/persistence/dto/request/account/UpdateUsernameRequest.java
@@ -2,13 +2,13 @@ package com.example.echo_api.persistence.dto.request.account;
 
 import com.example.echo_api.validation.Username;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 
 // @formatter:off
 public record UpdateUsernameRequest(
 
     @Username
-    @NotEmpty(message = "Username is required.")
+    @NotBlank(message = "Username is required.")
     String username
 
 ) {}
