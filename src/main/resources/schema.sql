1a3aac61d0bf80de0e26ab6b207d6e1778a99152
Ben Allenden <benallenden@protonmail.com>
2025-01-25 03:02:52 +0000

    Update user password table column name

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE
     IF NOT EXISTS "user" (
-        id          UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-        username    VARCHAR(255) UNIQUE NOT NULL CHECK (username ~ '^[a-zA-Z0-9_]{3,15}$'),
-        password    VARCHAR(255),
-        role        VARCHAR(255) NOT NULL DEFAULT USER,
-        enabled     BOOLEAN NOT NULL DEFAULT TRUE,
-        created_at  TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        updated_at  TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+        id                  UUID PRIMARY KEY UNIQUE NOT NULL DEFAULT gen_random_uuid(),
+        username            VARCHAR(255) UNIQUE NOT NULL CHECK (username ~ '^[a-zA-Z0-9_]{3,15}$'),
+        encrypted_password  VARCHAR(255),
+        role                VARCHAR(255) NOT NULL DEFAULT USER,
+        enabled             BOOLEAN NOT NULL DEFAULT TRUE,
+        created_at          TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at          TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
     );
 
 CREATE UNIQUE INDEX 
