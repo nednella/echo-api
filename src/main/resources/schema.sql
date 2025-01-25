ee5c271e2346d4adfe467a7350e8128465f0db04
Ben Allenden <benallenden@protonmail.com>
2025-01-25 03:24:18 +0000

    Add missing user.password NOT NULL

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE
     IF NOT EXISTS "user" (
         id                  UUID PRIMARY KEY UNIQUE NOT NULL DEFAULT gen_random_uuid(),
         username            VARCHAR(255) UNIQUE NOT NULL CHECK (username ~ '^[a-zA-Z0-9_]{3,15}$'),
-        encrypted_password  VARCHAR(255),
+        encrypted_password  VARCHAR(255) NOT NULL,
         role                VARCHAR(255) NOT NULL DEFAULT USER,
         enabled             BOOLEAN NOT NULL DEFAULT TRUE,
         created_at          TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
