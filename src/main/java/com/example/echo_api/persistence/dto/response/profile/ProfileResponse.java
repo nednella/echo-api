e7574a6ce55defb7e0c045466c5dcff97c089fc4
Ben Allenden <benallenden@protonmail.com>
2025-01-25 17:06:47 +0000

    Update json property names

--- src/main/java/com/example/echo_api/persistence/dto/response/profile/ProfileResponse.java
+++ src/main/java/com/example/echo_api/persistence/dto/response/profile/ProfileResponse.java
@@ -2,17 +2,19 @@ package com.example.echo_api.persistence.dto.response.profile;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 // @formatter:off
 public record ProfileResponse(
     String username,
-    String displayName,
+    @JsonProperty("display_name") String displayName,
     String bio,
     String location,
-    String avatarUrl,
-    String bannerUrl,
-    int followingCount,
-    int followerCount,
-    String createdAt
+    @JsonProperty("avatar_url") String avatarUrl,
+    @JsonProperty("banner_url") String bannerUrl,
+    @JsonProperty("following_count") int followingCount,
+    @JsonProperty("follower_count") int followerCount,
+    @JsonProperty("created_at") String createdAt
 ) {
 
     public ProfileResponse(
