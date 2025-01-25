38a1e86e87200bef585bc493c4c88ea640272b7b
Ben Allenden <benallenden@protonmail.com>
2025-01-25 03:32:54 +0000

    Update formatting

--- src/main/java/com/example/echo_api/persistence/dto/response/error/ErrorResponse.java
+++ src/main/java/com/example/echo_api/persistence/dto/response/error/ErrorResponse.java
@@ -8,25 +8,29 @@ import org.springframework.http.HttpStatus;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+// @formatter:off
 @JsonInclude(Include.NON_NULL)
 public record ErrorResponse(
     String timestamp,
     int status,
     String message,
     String details,
-    String path) {
+    String path
+) {
 
     public ErrorResponse(
         HttpStatus status,
         String message,
         String details,
-        String path) {
+        String path
+    ) {
         this(
             Instant.now().toString(),
             status.value(),
             message,
             details,
-            path);
+            path
+        );
     }
 
     /**
@@ -50,3 +54,4 @@ public record ErrorResponse(
     }
 
 }
+// @formatter:on
