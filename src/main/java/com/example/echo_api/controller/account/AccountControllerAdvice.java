d9c7f2172abc339f9417441465029b370e507de9
Ben Allenden <benallenden@protonmail.com>
2025-01-25 03:32:37 +0000

    Separate method formatter off/on

--- src/main/java/com/example/echo_api/controller/account/AccountControllerAdvice.java
+++ src/main/java/com/example/echo_api/controller/account/AccountControllerAdvice.java
@@ -3,6 +3,7 @@ package com.example.echo_api.controller.account;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 import com.example.echo_api.exception.AbstractControllerAdvice;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
