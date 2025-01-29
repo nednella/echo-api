94a6e8638bd5c1f830c0f084bece7fd088bc534d
Ben Allenden <benallenden@protonmail.com>
2025-01-29 02:24:47 +0000

    Implement SocialContextService follow/unfollow logic

--- src/main/java/com/example/echo_api/service/socialcontext/SocialContextServiceImpl.java
+++ src/main/java/com/example/echo_api/service/socialcontext/SocialContextServiceImpl.java
@@ -1,9 +1,14 @@
 package com.example.echo_api.service.socialcontext;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.echo_api.exception.custom.socialcontext.AlreadyFollowingException;
+import com.example.echo_api.exception.custom.socialcontext.NotFollowingException;
 import com.example.echo_api.persistence.model.follow.Follow;
 import com.example.echo_api.persistence.model.profile.Profile;
+import com.example.echo_api.persistence.repository.FollowRepository;
+import com.example.echo_api.service.metrics.MetricsService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,29 +17,48 @@ import lombok.RequiredArgsConstructor;
  * relationships.
  * 
  * @see Follow
+ * @see FollowRepository
  */
 @Service
 @RequiredArgsConstructor
 public class SocialContextServiceImpl implements SocialContextService {
 
+    private final MetricsService metricsService;
+
+    private final FollowRepository followRepository;
+
     @Override
+    @Transactional
     public void follow(Profile source, Profile target) {
-        throw new UnsupportedOperationException("Unimplemented method 'follow'");
+        if (isFollowing(source, target)) {
+            throw new AlreadyFollowingException();
+        }
+
+        metricsService.incrementFollowing(source.getProfileId());
+        metricsService.incrementFollowers(target.getProfileId());
+        followRepository.save(new Follow(source.getProfileId(), target.getProfileId()));
     }
 
     @Override
+    @Transactional
     public void unfollow(Profile source, Profile target) {
-        throw new UnsupportedOperationException("Unimplemented method 'unfollow'");
+        if (!isFollowing(source, target)) {
+            throw new NotFollowingException();
+        }
+
+        metricsService.decrementFollowing(source.getProfileId());
+        metricsService.decrementFollowers(target.getProfileId());
+        followRepository.delete(new Follow(source.getProfileId(), target.getProfileId()));
     }
 
     @Override
     public boolean isFollowing(Profile source, Profile target) {
-        throw new UnsupportedOperationException("Unimplemented method 'isFollowing'");
+        return followRepository.existsByFollowerIdAndFollowingId(source.getProfileId(), target.getProfileId());
     }
 
     @Override
     public boolean isFollowedBy(Profile source, Profile target) {
-        throw new UnsupportedOperationException("Unimplemented method 'isFollowedBy'");
+        return isFollowing(target, source);
     }
 
 }
