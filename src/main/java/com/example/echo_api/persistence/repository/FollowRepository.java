fb30fbaca816eeaddd00956cb43fea25858cb896
Ben Allenden <benallenden@protonmail.com>
2025-01-29 02:15:40 +0000

    Add FollowRepository check if exists method

--- src/main/java/com/example/echo_api/persistence/repository/FollowRepository.java
+++ src/main/java/com/example/echo_api/persistence/repository/FollowRepository.java
@@ -1,10 +1,14 @@
 package com.example.echo_api.persistence.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.repository.ListCrudRepository;
 
 import com.example.echo_api.persistence.model.follow.Follow;
 import com.example.echo_api.persistence.model.follow.FollowPK;
 
 public interface FollowRepository extends ListCrudRepository<Follow, FollowPK> {
+
+    boolean existsByFollowerIdAndFollowingId(UUID followerId, UUID followingId);
 
 }
